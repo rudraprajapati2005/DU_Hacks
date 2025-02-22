610c322ca0de4882fc2f77526e318b363a63aafd
unknown <rudraprajapati5102005@gmail.com>
2025-02-22 12:38:26 +0530

    Commit Message

--- ClassHub/ClassHubApp/models.py
+++ ClassHub/ClassHubApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ClassroomCreator(models.Model):
+    user_id = models.TextField(primary_key=True)
+    user_classroom_name = models.CharField(max_length=255,unique=False)
+    user_email = models.CharField(max_length=255,unique=True)
+    user_password= models.TextField( null=True)  # Optional field for biography
+
+class Classroom(models.Model):
+    creator = models.ForeignKey(ClassroomCreator, on_delete=models.CASCADE)
+    classroom_name = models.CharField(max_length=255,unique=True)
+    

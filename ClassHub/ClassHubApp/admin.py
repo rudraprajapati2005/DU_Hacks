39e03c168a1ba3437a2a9bb0bb6ff7c9b878e02f
Jaiminjraval <jayminraval2042006@gmail.com>
2025-02-22 14:39:12 +0530

    Done

--- ClassHub/ClassHubApp/admin.py
+++ ClassHub/ClassHubApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student, Teacher_data, ClassroomCreator, Classroom
+
+admin.site.register(Student)
+admin.site.register(Teacher_data)
+admin.site.register(ClassroomCreator)
+admin.site.register(Classroom)
 
 # Register your models here.

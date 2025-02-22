ac47f187e4bd7515daa2f9fc5a30d0a253940f8c
meetprajap <meetp5986@gmail.com>
2025-02-22 12:42:28 +0530

    login

--- ClassHub/ClassHub/urls.py
+++ ClassHub/ClassHub/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from ClassHub import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ClassHubApp.urls'))
+    path('', include('ClassHubApp.urls')),
+    path('',views.login),
+    path('register/',views.register),
 ]
--- ClassHub/ClassHubApp/views.py
+++ ClassHub/ClassHubApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from investor.models import investment_data  # Importing from investor app
+from entrepreneur.models import entrepreneur_data  # Importing from entrepreneur app
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages
 
+def home(request):
+    
 # Create your views here.
